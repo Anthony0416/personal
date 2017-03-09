b5afdcfb769222cb9829b9e8e7c00d6e8f689110
chentongtong <csw0416@hotmail.com>
2017-03-09 11:05:45 +0800

    首页完善

--- app/src/pages/welcome.jsx
+++ app/src/pages/welcome.jsx
@@ -11,7 +11,19 @@ export default class Login extends React.Component {
 
             <div id="welcomeWrap" style={{backgroundImage: 'url("https://bing.ioliu.cn/v1/rand")'}}>
                 <div id="contentWrap">
-                    <p style={{textAlign: 'center',fontSize: '20'}}>个人网站</p>
+                    <div style={{padding: '0 0 20px 0', borderBottom: '1px solid #eee'}}>
+                        <p style={{textAlign: 'center',fontSize: '20px'}}>陈世文的个人网站</p>
+                        <p style={{textAlign: 'center',fontSize: '16px'}}>Chen Shiwen's Personal Website</p>
+                    </div>
+                    <p id="link">
+                        <a>Blog</a>&nbsp;/&nbsp;
+                        <a>Github</a>&nbsp;/&nbsp;
+                        <a href="http://weibo.com/u/3798317205">Microblog</a>&nbsp;/&nbsp;
+                        <a>About</a>
+                    </p>
+                    <p id="last">
+                        <a>Content</a>&nbsp;|&nbsp;chenshiwen.top
+                    </p>
                 </div>
             </div>
         );
